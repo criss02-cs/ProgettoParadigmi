d8bdb86b3d18b6c39bbc0b844f84f3317087de31
CristianoMac <cristianoaloigi0@gmail.com>
2023-12-29 21:02:08 +0100

    cambiato modo di generare il colore casuale

--- ProgettoParadigmi.Api/Utils/ColorUtils.cs
+++ ProgettoParadigmi.Api/Utils/ColorUtils.cs
@@ -7,10 +7,10 @@ public static class ColorUtils
     public static string GetRandomColor()
     {
         var random = new Random();
-        var color = Color.FromArgb(0, random.Next(256), 
+        var color = Color.FromArgb(1, random.Next(256), 
             random.Next(256), random.Next(256));
         var hexColor =
-            $"#{color.A:X}{color.R:X}{color.G:X}{color.B:X}";
+            $"#{color.R:X}{color.G:X}{color.B:X}";
         return hexColor;
     }
 }
