83bcce6c3dc5481dad00a52f5a452779fadd0d89
CristianoMac <cristianoaloigi0@gmail.com>
2023-12-29 22:02:07 +0100

    sistemate stringhe di connessione

--- ProgettoParadigmi.Api/Program.cs
+++ ProgettoParadigmi.Api/Program.cs
@@ -14,11 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppuntamentiDbContext>(opt =>
     // opt.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
-#if DEBUG
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")
-#else
-    opt.UseSqlServer(builder.Configuration["ConnectionStrings-DefaultConnection"]
-#endif
     ));
 
 builder.Services.AddControllers();
